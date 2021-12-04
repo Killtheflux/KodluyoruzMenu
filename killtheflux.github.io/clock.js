eb8c84a74685a844f1fd537cf61f35931acce637
Hasret Cem <hasretcembicer@gmail.com>
2021-12-04 17:41:00 +0300

    initial

---
+++ killtheflux.github.io/clock.js
@@ -0,0 +1,20 @@
+let myName = prompt("Kullanici Adinizi Giriniz:")
+document.getElementById("myName").innerHTML = myName;
+
+function showTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("myClock").innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(showTime, 1000);
+  
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+showTime()
